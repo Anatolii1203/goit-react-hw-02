749bbe38a13094c2a8e46767e67dd1846589f564
Anatolii1203 <v120386t@gmail.com>
2024-07-28 12:25:18 +0300

    react-02

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import s from "./App.module.css";
+import { useEffect, useState } from "react";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
+
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedData = JSON.parse(window.localStorage.getItem("feedback"));
+    if (savedData) {
+      return savedData;
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  const { good, neutral, bad } = feedback;
+  const totalFeedback = good + neutral + bad;
+  const positive = Math.round((good / totalFeedback) * 100);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
+
+  const handleReset = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  return (
+    <div className={s.container}>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        handleReset={handleReset}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positive={positive}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,17 @@
+import s from "./Feedback.module.css";
+
+const Feedback = ({ feedback, totalFeedback, positive }) => {
+  const { good, neutral, bad } = feedback;
+
+  return (
+    <div className={s.container}>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positive}%</p>
+    </div>
+  );
+};
+
+export default Feedback;
